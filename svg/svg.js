ba9e75a27acb092d25297a2f4279da37c84826f2
catalin619 <56998762+catalin619@users.noreply.github.com>
2020-02-01 12:38:23 +0200

    Add files via upload

--- svg/svg.js
+++ svg/svg.js
@@ -1,6 +1,30 @@
-document.getElementsByClassName("id_logic_version").innerHTML = "Logic version = 2019.12.06.0"
-window.addEventListener("deviceorienation", on_orentation_uab);
-window.addEventListener("devicemotion", on_motion_uab);
+document.getElementsByClassName("id_logic_version").innerHTML = "Logic version = 2019.12.06.0";
+document.getElementById("id_button").addEventListener("click", on_cere_permisiuni);
+if (typeof(DeviceOrienationEvent.requestPermission) == "function"){
+
+}
+else
+      window.addEventListener("deviceorienation", on_orentation_uab);
+  window.addEventListener("devicemotion", on_motion_uab);
+
+  function f_ok(p)
+  {
+    if (p == "granted")
+      window.addEventListener("deviceorienation", on_orentation_uab);
+    else
+      alert("Permisiune respinsa");
+  }
+
+function f_not_ok(p)
+  {
+      alert("Permisiune respinsa");
+
+  }
+
+function on_cere_permisiuni()
+{
+    DeviceOrienationEvent.requestPermission().then(f_ok).catch(f_not_ok);
+}
 
 function desenare(beta,gamma)
 {
@@ -18,15 +42,15 @@ function desenare(beta,gamma)
     cerc.setAttribute("cy", y);
 }
 
-// function on_orentation_uab(e)
-// {
-//   document.getElementById("id_alpha").innerHTML = Math.round(e.alpha * 100)/100;
-//   document.getElementById("id_beta").innerHTML = Math.round(e.beta * 100)/100;
-//   document.getElementById("id_gamma").innerHTML = Math.round(e.gamma * 100)/100;
-//
-//   desenare(e.beta,e.gamma);
-// }
+ function on_orentation_uab(e)
+ {
+   document.getElementById("id_alpha").innerHTML = Math.round(e.alpha * 100)/100;
+   document.getElementById("id_beta").innerHTML = Math.round(e.beta * 100)/100;
+   document.getElementById("id_gamma").innerHTML = Math.round(e.gamma * 100)/100;
 
+   desenare(e.beta,e.gamma);
+ }
+/*
 function on_montion_uab(e)
 {
   var acc = e.accelerationIncludingGravity;
@@ -40,5 +64,6 @@ function on_montion_uab(e)
   document.getElementById("id_beta_acc").innerHTML = Mah.round(beta * 100)/100;
   document.getElementById("id_gamma_acc").innerHTML = Math.round(gamma * 100)/100;
 
-  //desenare(beta, gamma);
+  desenare(beta, gamma);
 }
+*/
