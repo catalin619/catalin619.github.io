2b84da7016372d939af1bc9c93260c9e711c7b3e
catalin619 <56998762+catalin619@users.noreply.github.com>
2020-02-02 19:22:45 +0200

    Add files via upload

---
+++ Examen/aplicatie.js
@@ -0,0 +1,41 @@
+const canvas = document.querySelector('#id_canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+onload = setInterval(randomize, 500);
+var numara = 0;
+var panaLa = Math.floor(Math.random()*11);
+
+
+function randomize(){
+  var rr = 10;
+  var rx = Math.ceil(290* Math.random());
+  var ry = Math.ceil(290* Math.random());
+  numara ++;
+  drawCircle(rx,ry,rr);
+  if(numara > panaLa)
+  {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
+function drawCircle(rx,ry,rr){
+  var myColors  =["purple","red"];
+  var colorPicker = Math.ceil(4* Math.random() -1);
+  ctx.strokeStyle = myColors[colorPicker];
+  ctx.beginPath();
+  ctx.arc(rx,ry,rr,0,2*Math.PI);
+  ctx.stroke();
+  ctx.closePath();
+}
+
+  function verifica(){
+    var numar = document.getElementById('cercuri').value;
+  if(numar == panaLa)
+  {
+    alert("Corect!");
+  }
+  else {
+    alert("Incorect!");
+  }
+}
